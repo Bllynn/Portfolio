533c96b1b095114f10854f6b7a927ad82e38b9a0
Brandon Griffith <BrandonGri5th@gmail.com>
2019-01-08 13:36:13 -0700

    Carousel added nto working.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+      This is going to be my portfolio project that I should've had done MONTHS ago...this is to new begininings.
       </div>
     );
   }
