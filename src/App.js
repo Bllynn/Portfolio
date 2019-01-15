e366c819a10f3dc4d3fa40ec8347e10aedc8e317
Brandon Griffith <BrandonGri5th@gmail.com>
2019-01-14 21:51:41 -0700

    y

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Contact from "./Contact";
+import pillars from "./pillars_of_creation-t2.jpg";
+import pinwheel from "./pinwheel_galaxy.jpg";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="background-image1">
+        <div className="background-image1" backgroundImage={pillars}>
           <Navbar />
           <div className="intro">
             <h1>Brandon Griffith</h1>
             <p>
-              I am driven by my love of learning, and strive to better myself on
+              I need to write a chunk of text that actually demonstrates what
+              actual text on a page might look like. With proper spacing and
+              indentation, we can see that the words flow on the page a certain
+              way.
+              {/* I am driven by my love of learning, and strive to better myself on
               a daily basis. I have been recognized for my excellent customer
               service, and ability to help people solve problems. I strive to
               make myself a valuable asset to the team through hard work, while
-              maintaining positive energy with great communication skills.
+              maintaining positive energy with great communication skills. */}
             </p>
           </div>
         </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,28 +1,72 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
+  state = { form: {} };
+
+  handleSubmit = event => {
+    const { name, email, subject, message } = this.state.form;
+    alert(`${name},${email},${subject},${message}`);
+  };
+  handleChange = event => {
+    let form = this.state.form;
+    let name = event.target.name;
+    let value = event.target.value;
+    form[name] = value;
+    this.setState({ form });
+  };
+
   render() {
     return (
       <div className="contact-container">
-      <h1>Contact Me</h1>
-        <div className="contact-form">
-          <span>
-            <label for="name">Name</label>
-            <input type="text" id="name" placeholder="Who are you?" />
-          </span>
-          <span>
-            <label for="email">E-Mail</label>
-            <input type="text" id="email" placeholder="What's YOUR email?" />
-          </span>
-          <span>
-            <label for="subject">Subject</label>
-            <input type="text" id="subject" placeholder="What is the offer?" />
-          </span>
-          <span>
-            <label for="message">Message</label>
-            <input type="t" id="message" placeholder="YES!" />
-          </span>
-        </div>
+        <h1>Contact Me</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="contact-form">
+            <span>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                onChange={this.handleChange}
+                id="name"
+                name="name"
+                value={this.state.name}
+                placeholder="Name"
+              />
+            </span>
+            <span>
+              <label htmlFor="email">E-Mail</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                value={this.state.email}
+                placeholder="Email"
+              />
+            </span>
+            <span>
+              <label htmlFor="subject">Subject</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                onChange={this.handleChange}
+                value={this.state.subject}
+                placeholder="Subject"
+              />
+            </span>
+            <span>
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                onChange={this.handleChange}
+                value={this.state.message}
+                placeholder="Message"
+              />
+            </span>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
       </div>
     );
   }
